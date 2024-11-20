db67fc3dfff17a28eb1fb2effb0bd2c8a99e192a
huayonghong <hua.yonghong@outlook.com>
2024-11-20 12:16:17 +0800

    motor and robot match to jetson orin nano, change the I2C bus ID to 7, istead of jetson nano 1

--- jetbot/motor.py
+++ jetbot/motor.py
@@ -31,7 +31,7 @@
 # SOFTWARE.
 #==================================================================================
 import atexit
-import qwiic
+# import qwiic
 from Adafruit_MotorHAT import Adafruit_MotorHAT
 import traitlets
 from traitlets.config.configurable import Configurable
@@ -39,7 +39,7 @@ from traitlets.config.configurable import Configurable
 
 
 # Scan for devices on I2C bus
-addresses = qwiic.scan()
+# addresses = qwiic.scan()
 
 class Motor(Configurable):
 
@@ -51,77 +51,77 @@ class Motor(Configurable):
     beta = traitlets.Float(default_value=0.0).tag(config=True)
     
     # Adafruit Hardware
-    if 96 in addresses:
+    # if 96 in addresses:
+
+    def __init__(self, driver, channel, *args, **kwargs):
+        super(Motor, self).__init__(*args, **kwargs)  # initializes traitlets
+
+        self._driver = driver
+        self._motor = self._driver.getMotor(channel)
+        if(channel == 1):
+            self._ina = 1
+            self._inb = 0
+        else:
+            self._ina = 2
+            self._inb = 3
+        atexit.register(self._release)
         
-        def __init__(self, driver, channel, *args, **kwargs):
-            super(Motor, self).__init__(*args, **kwargs)  # initializes traitlets
+    @traitlets.observe('value')
+    def _observe_value(self, change):
+        self._write_value(change['new'])
 
-            self._driver = driver
-            self._motor = self._driver.getMotor(channel)
-            if(channel == 1):
-                self._ina = 1
-                self._inb = 0
-            else:
-                self._ina = 2
-                self._inb = 3
-            atexit.register(self._release)
-            
-        @traitlets.observe('value')
-        def _observe_value(self, change):
-            self._write_value(change['new'])
-
-        def _write_value(self, value):
-            """Sets motor value between [-1, 1]"""
-            mapped_value = int(255.0 * (self.alpha * value + self.beta))
-            speed = min(max(abs(mapped_value), 0), 255)
-            self._motor.setSpeed(speed)
-            if mapped_value < 0:
-                self._motor.run(Adafruit_MotorHAT.FORWARD)
-                # The two lines below are required for the Waveshare JetBot Board only
-                self._driver._pwm.setPWM(self._ina,0,0)
-                self._driver._pwm.setPWM(self._inb,0,speed*16)
-            else:
-                self._motor.run(Adafruit_MotorHAT.BACKWARD)
-                # The two lines below are required for the Waveshare JetBot Board only
-                self._driver._pwm.setPWM(self._ina,0,speed*16)
-                self._driver._pwm.setPWM(self._inb,0,0)
-
-        def _release(self):
-            """Stops motor by releasing control"""
-            self._motor.run(Adafruit_MotorHAT.RELEASE)
+    def _write_value(self, value):
+        """Sets motor value between [-1, 1]"""
+        mapped_value = int(255.0 * (self.alpha * value + self.beta))
+        speed = min(max(abs(mapped_value), 0), 255)
+        self._motor.setSpeed(speed)
+        if mapped_value < 0:
+            self._motor.run(Adafruit_MotorHAT.FORWARD)
             # The two lines below are required for the Waveshare JetBot Board only
             self._driver._pwm.setPWM(self._ina,0,0)
-            self._driver._pwm.setPWM(self._inb,0,0)    
+            self._driver._pwm.setPWM(self._inb,0,speed*16)
+        else:
+            self._motor.run(Adafruit_MotorHAT.BACKWARD)
+            # The two lines below are required for the Waveshare JetBot Board only
+            self._driver._pwm.setPWM(self._ina,0,speed*16)
+            self._driver._pwm.setPWM(self._inb,0,0)
+
+    def _release(self):
+        """Stops motor by releasing control"""
+        self._motor.run(Adafruit_MotorHAT.RELEASE)
+        # The two lines below are required for the Waveshare JetBot Board only
+        self._driver._pwm.setPWM(self._ina,0,0)
+        self._driver._pwm.setPWM(self._inb,0,0)    
 
     # SparkFun Hardware
-    elif 93 in addresses:
+    # elif 93 in addresses:
 
-        def __init__(self, driver, channel, *args, **kwargs):
-            super(Motor, self).__init__(*args, **kwargs)  # initializes traitlets
+    #     def __init__(self, driver, channel, *args, **kwargs):
+    #         super(Motor, self).__init__(*args, **kwargs)  # initializes traitlets
 
-            self._driver = driver
-            atexit.register(self._release)
-            self.channel = channel
+    #         self._driver = driver
+    #         atexit.register(self._release)
+    #         self.channel = channel
             
-        @traitlets.observe('value')
-        def _observe_value(self, change):
-            self._write_value(change['new'])
+    #     @traitlets.observe('value')
+    #     def _observe_value(self, change):
+    #         self._write_value(change['new'])
 
-        def _write_value(self, value):
-            """Sets motor value between [-1, 1]"""
-            speed = int(255 * (self.alpha * value + self.beta))
+    #     def _write_value(self, value):
+    #         """Sets motor value between [-1, 1]"""
+    #         speed = int(255 * (self.alpha * value + self.beta))
 
-            # Set Motor Controls: .set_drive( motor number, direction, speed)
-            # Motor Number: A = 0, B = 1
-            # Direction: FWD = 0, BACK = 1
-            # Speed: (-255) - 255 (neg. values reverse direction of motor)
+    #         # Set Motor Controls: .set_drive( motor number, direction, speed)
+    #         # Motor Number: A = 0, B = 1
+    #         # Direction: FWD = 0, BACK = 1
+    #         # Speed: (-255) - 255 (neg. values reverse direction of motor)
 
-            if self.channel == 1:
-                self._motor = self._driver.set_drive(self.channel-1, 0, speed)
-            elif self.channel == 2:
-                self._motor = self._driver.set_drive(self.channel-1, 0, speed)
-            self._driver.enable()
+    #         if self.channel == 1:
+    #             self._motor = self._driver.set_drive(self.channel-1, 0, speed)
+    #         elif self.channel == 2:
+    #             self._motor = self._driver.set_drive(self.channel-1, 0, speed)
+    #         self._driver.enable()
                 
-        def _release(self):
-            """Stops motor by releasing control"""
-            self._driver.disable()
+    #     def _release(self):
+    #         """Stops motor by releasing control"""
+    #         self._driver.disable()
